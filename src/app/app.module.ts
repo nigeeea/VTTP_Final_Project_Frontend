a8735a2a3b52eb191bd102efab55bd9459d9d488
Nigel <nigeltjk1@gmail.com>
2023-02-26 23:35:38 +0800

    first push for vttp project frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login.component';
+import { SignupComponent } from './component/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
